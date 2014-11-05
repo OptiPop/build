d9c6d5c75dfe0ff5d3f497b3f71546203be71ce4
Chet Kener <Cl3Kener@gmail.com>
2014-11-04 23:07:28 -0500

    Remove Annoying Goldfish Stuff
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- target/product/emulator.mk
+++ target/product/emulator.mk
@@ -25,27 +25,12 @@ PRODUCT_PACKAGES += \
 # Device modules
 PRODUCT_PACKAGES += \
     egl.cfg \
-    gralloc.goldfish \
     libGLESv1_CM_emulation \
     lib_renderControl_enc \
     libEGL_emulation \
-    libGLES_android \
     libGLESv2_enc \
     libOpenglSystemCommon \
     libGLESv2_emulation \
     libGLESv1_enc \
     qemu-props \
-    qemud \
-    camera.goldfish \
-    camera.goldfish.jpeg \
-    lights.goldfish \
-    gps.goldfish \
-    sensors.goldfish \
-    e2fsck
-
-
-PRODUCT_COPY_FILES += \
-    device/generic/goldfish/fstab.goldfish:root/fstab.goldfish \
-    device/generic/goldfish/init.goldfish.rc:root/init.goldfish.rc \
-    device/generic/goldfish/init.goldfish.sh:system/etc/init.goldfish.sh \
-    device/generic/goldfish/ueventd.goldfish.rc:root/ueventd.goldfish.rc
+    qemud
