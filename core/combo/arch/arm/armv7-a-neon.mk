b31dc39cad151ddd2dc0c5c621c1a852758bcc56
Chet Kener <Cl3Kener@gmail.com>
2014-12-13 09:37:17 -0500

    armv7-a: More Specific Tunings per CPU
    * Also add some substitutions and ignores for clang.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -10,26 +10,27 @@ ifneq (,$(filter cortex-a15 krait denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VA
 	# TODO: krait is not a cortex-a15, we set the variant to cortex-a15 so that
 	#       hardware divide operations are generated. This should be removed and a
 	#       krait CPU variant added to GCC/clang.
-	arch_variant_cflags := -mcpu=cortex-a15
+	arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4 -mvectorize-with-neon-quad
 
-	# Fake an ARM compiler flag as these processors support LPAE which GCC/clang
-	# don't advertise.
-	arch_variant_cflags += -D__ARM_FEATURE_LPAE=1
+	# Fake ARM compiler flags as these processors support LPAE and VFP4 which GCC/clang
+	# doesn't advertise.
+	arch_variant_cflags += -D__ARM_FEATURE_LPAE=1 -D__ARM_FEATURE_VFP4=1
 else
-ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a8)
-	arch_variant_cflags := -mcpu=cortex-a8
+ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
+	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon-vfpv3
+else
+ifneq (,$(filter cortex-a8 scorpion,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+	arch_variant_cflags := -mcpu=cortex-a8 -mfpu=neon
+	arch_variant_ldflags := -Wl,--fix-cortex-a8 
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
-	arch_variant_cflags := -mcpu=cortex-a7
+	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon
 else
-	arch_variant_cflags := -march=armv7-a
+	arch_variant_cflags := -march=armv7-a -mfpu=neon
+endif
 endif
 endif
 endif
 
 arch_variant_cflags += \
-    -mfloat-abi=softfp \
-    -mfpu=neon
-
-arch_variant_ldflags := \
-	-Wl,--fix-cortex-a8
+    -mfloat-abi=softfp
