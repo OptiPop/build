fae556b38955c12c8da903bad93daea02146ea26
Lars Greiss <kufikugel@googlemail.com>
2014-12-16 16:39:54 +0100

    Build: slim up sounds and update (2/2)
    reference on sdk and full base builds to slims make file.
    As well remove both copy command in sdk build. This is handled already on slims
    make file.
    
    Change-Id: I9b0146df1a43cfc5282288012fc59509e0badedf
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -40,7 +40,7 @@ PRODUCT_LOCALES := en_US
 PRODUCT_AAPT_CONFIG := normal
 
 # Get some sounds
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage13_48.mk)
+$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudioSlim.mk)
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
