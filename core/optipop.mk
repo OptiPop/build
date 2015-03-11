9fda88ea59f32c90bbe2bfe85c2b3edd9e129dbf
Chet Kener <Cl3Kener@gmail.com>
2015-03-10 23:52:56 -0400

    Fix Graphite Compile
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- core/optipop.mk
+++ core/optipop.mk
@@ -369,7 +369,8 @@ LOCAL_DISABLE_GRAPHITE := \
 	libwebviewchromium_plat_support \
 	libjni_filtershow_filters \
 	fio \
-	libwebrtc_spl
+	libwebrtc_spl \
+	libpcap
 
 ifneq (1,$(words $(filter $(LOCAL_DISABLE_GRAPHITE), $(LOCAL_MODULE))))
 ifdef LOCAL_CONLYFLAGS
