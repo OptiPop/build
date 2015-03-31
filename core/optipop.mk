4a79bda70b8cf540fa7a6d0e0e4ea6347f27a4f2
Maxime Poulain <maxime.poulain@student.uclouvain.be>
2015-03-31 21:43:32 +0200

    optipop.mk:ffast-math:initial introduction

--- core/optipop.mk
+++ core/optipop.mk
@@ -429,3 +429,75 @@ endif
 endif
 endif
 #####
+####################
+# FORCE FFAST-MATH #
+####################
+ifeq ($(FFAST_MATH),true)
+LOCAL_FORCE_FFAST_MATH := \
+	libskia \
+	libGLESv2 \
+	libEGL \
+	libGLESv1_CM \
+	libGLES_android \
+	skia_skia_gyp \
+	ui_gfx_gfx_gyp \
+	ui_gfx_ipc_gfx_ipc_gyp \
+	ui_gl_gl_gyp \
+	libui \
+	libgui \
+	ui_base_ui_base_gyp \
+	ui_gfx_gfx_geometry_gyp \
+	third_party_WebKit_Source_core_webcore_rendering_gyp \
+	third_party_WebKit_Source_core_webcore_svg_gyp \
+	third_party_WebKit_Source_core_webcore_generated_gyp \
+	third_party_WebKit_Source_core_webcore_html_gyp \
+	third_party_WebKit_Source_core_webcore_remaining_gy \
+	third_party_WebKit_Source_web_blink_web_gyp \
+	gpu_gl_in_process_context \
+	cc_cc_gyp
+
+LOCAL_DISABLE_SINGLE_PRECISION :=
+
+#########
+# To Try#
+#########
+
+#        libstagefright_color_conversion \
+#        libstagefright_aacenc \
+#        libstagefright_matroska \
+#        libstagefright_webm \
+#        libstagefright_timedtext \
+#        libvpx \
+#        libwebm \
+#        libstagefright_mpeg2ts \
+#        libstagefright_id3 \
+#        libFLAC \
+#        libmedia_helper
+#        skia_skia_gyp
+#    	ui_gfx_gfx_gyp \
+#		ui_gfx_gfx_geometry_gyp \
+#       ui_gfx_ipc_gfx_ipc_gyp
+#
+
+ifneq ($(filter $(LOCAL_FORCE_FFAST_MATH), $(LOCAL_MODULE)),)
+ifdef LOCAL_CONLYFLAGS
+LOCAL_CONLYFLAGS += -ffast-math -ftree-vectorize
+else
+LOCAL_CONLYFLAGS := -ffast-math -ftree-vectorize
+endif
+
+ifdef LOCAL_CPPFLAGS
+LOCAL_CPPFLAGS +=  -ffast-math -ftree-vectorize
+else
+LOCAL_CPPFLAGS :=  -ffast-math -ftree-vectorize
+endif
+
+### Some modules doesn't like forcing single precision, until we fix casting errors, let's disable this optimization
+ifeq ($(filter $(LOCAL_DISABLE_SINGLE_PRECISION), $(LOCAL_MODULE)),)
+LOCAL_CONLYFLAGS += -fsingle-precision-constant
+LOCAL_CPPFLAGS   += -fsingle-precision-constant
+endif
+
+endif
+endif
+#####
