c8a1ef02b4bcff89ecfb919b0fbb8a013f7fc6f0
Maxime Poulain <maxime.poulain@student.uclouvain.be>
2015-04-23 07:08:50 +0200

    optipop.mk:squashed few disable modules

--- core/optipop.mk
+++ core/optipop.mk
@@ -253,7 +253,6 @@ WEBCHROMIUM_STATIC_LIBRARIES := \
 ####################
 
 LOCAL_DISABLE_O3 := \
-    libstagefright \
     libstagefright_soft_aacdec \
     libstagefright_id3 \
     libbluetooth_jni \
@@ -302,18 +301,62 @@ LOCAL_DISABLE_PIPE := \
 #####################
 
 LOCAL_DISABLE_STRICT := \
-    libc_malloc \
+    third_party_libyuv_libyuv_gyp \
+    third_party_WebKit_Source_wtf_wtf_gyp \
+    ipc_ipc_gyp \
+    third_party_webrtc_base_rtc_base_gyp \
+    courgette_courgette_lib_gyp \
+    third_party_WebKit_Source_platform_blink_common_gyp \
+    cc_cc_surfaces_gyp \
+    net_http_server_gyp \
+    base_base_gyp \
+    ui_gfx_gfx_gyp \
+    android_webview_native_webview_native_gyp \
+    jingle_jingle_glue_gyp \
+    ui_native_theme_native_theme_gyp \
+    third_party_WebKit_Source_core_webcore_dom_gyp \
+    third_party_WebKit_Source_core_webcore_html_gyp \
+    third_party_WebKit_Source_core_webcore_rendering_gyp \
+    third_party_WebKit_Source_core_webcore_svg_gyp \
+    components_autofill_content_browser_gyp \
+    ui_surface_surface_gyp \
+    printing_printing_gyp \
+    third_party_WebKit_Source_web_blink_web_gyp \
+    third_party_webrtc_modules_media_file_gyp \
+    cc_cc_gyp \
+    storage_storage_gyp \
+    android_webview_android_webview_common_gyp \
+    content_content_browser_gyp \
+    content_content_common_gyp \
+    content_content_child_gyp \
+    third_party_webrtc_modules_webrtc_utility_gyp \
+    third_party_webrtc_modules_iLBC_gyp \
+    third_party_webrtc_modules_neteq_gyp \
+    third_party_webrtc_modules_audio_device_gyp\
+    third_party_webrtc_modules_rtp_rtcp_gyp \
+    components_data_reduction_proxy_browser_gyp \
+    libunwind \
     libc_bionic \
+    libc_malloc \
+    e2fsck \
+    mke2fs \
+    tune2fs \
+    mkfs.exfat \
+    fsck.exfat \
+    mount.exfat \
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs \
     libc_dns \
     libc_tzcode \
-    libziparchive \
     libtwrpmtp \
-    libfuse \
     libfusetwrp \
     libguitwrp \
     busybox \
+    toolbox \
+    clatd \
+    ip \
     libuclibcrpc \
-    libziparchive-host \
     libpdfiumcore \
     libandroid_runtime \
     libmedia \
@@ -322,19 +365,37 @@ LOCAL_DISABLE_STRICT := \
     bluetooth.default \
     logd \
     mdnsd \
+    libfuse \
+    libcutils \
+    liblog \
+    healthd \
+    adbd \
+    libunwind \
+    libsync \
+    libnetutils \
+    libusbhost \
+    libfs_mgr \
+    libvold \
     net_net_gyp \
     libstagefright_webm \
     libaudioflinger \
     libmediaplayerservice \
     libstagefright \
+    libstagefright_avcenc \
+    libstagefright_avc_common \
+    libstagefright_httplive \
+    libstagefright_rtsp \
+    sdcard \
     ping \
+    libnetlink \
     ping6 \
-    libdiskconfig \
-    libjavacore \
     libfdlibm \
     libvariablespeed \
     librtp_jni \
     libwilhelm \
+    debuggerd \
+    libbt-brcm_bta \
+    libbt-brcm_stack \
     libdownmix \
     libldnhncr \
     libqcomvisualizer \
@@ -343,9 +404,7 @@ LOCAL_DISABLE_STRICT := \
     libandroidfw \
     dnsmasq \
     static_busybox \
-    libwebviewchromium \
-    libwebviewchromium_loader \
-    libwebviewchromium_plat_support \
+    libstagefright_foundation \
     content_content_renderer_gyp \
     third_party_WebKit_Source_modules_modules_gyp \
     third_party_WebKit_Source_platform_blink_platform_gyp \
@@ -362,15 +421,16 @@ LOCAL_DISABLE_STRICT := \
     libart-compiler \
     oatdump \
     libart-disassembler \
-    linker \
-    camera.msm8084 \
     mm-vdec-omx-test \
     libziparchive-host \
     libziparchive \
     libdiskconfig \
     logd \
+    linker \
     libjavacore \
-    camera.msm8084
+    camera.msm8084 \
+    libmmcamera_interface \
+    camera.hammerhead
 
 DISABLE_STRICT := \
     -fno-strict-aliasing
@@ -504,7 +564,8 @@ LOCAL_DISABLE_GRAPHITE := \
     libwebrtc_spl \
     libpcap \
     libsigchain \
-    libFraunhoferAAC
+    libFraunhoferAAC \
+    libart-disassembler
 
 GRAPHITE_FLAGS := \
     -fgraphite \
