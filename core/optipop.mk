6e257d168a97a9af8b430b2c21c44d66430b35e3
DespairFactor <matthewalex@outlook.com>
2015-05-07 14:10:50 -0400

    optipop.mk: exclude tcpdump from strict

--- core/optipop.mk
+++ core/optipop.mk
@@ -437,7 +437,8 @@ LOCAL_DISABLE_STRICT := \
     libjavacore \
     camera.msm8084 \
     libmmcamera_interface \
-    camera.hammerhead
+    camera.hammerhead \
+	tcpdump
 
 DISABLE_STRICT := \
     -fno-strict-aliasing
